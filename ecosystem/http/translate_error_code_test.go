e9f5945332fda282c1a0d79efc05e2b2f80a068e
Calvin Lobo <calvin.lobo@hootsuite.com>
2022-06-23 10:02:06 -0400

    ecosystem/http: - Renamed SetDefaultErrorCode to SetDefaultErrorStatus - Added GetStatus to return the mapped HTTP status code from the error, if it exists.

--- ecosystem/http/translate_error_code_test.go
+++ ecosystem/http/translate_error_code_test.go
@@ -2,39 +2,49 @@ package simplehttp
 
 import (
 	"fmt"
-	"github.com/lobocv/simplerr"
-	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/lobocv/simplerr"
 )
 
 func TestTranslateErrorCode(t *testing.T) {
 
 	testCases := []struct {
-		err      error
-		expected HTTPStatus
+		err                error
+		expected           HTTPStatus
+		expectMappingFound bool
 	}{
-		{fmt.Errorf("something"), http.StatusInternalServerError},
-		{simplerr.New("something").Code(simplerr.CodeUnknown), http.StatusInternalServerError},
-		{simplerr.New("something").Code(simplerr.CodePermissionDenied), http.StatusForbidden},
-		{simplerr.New("something").Code(simplerr.CodeCanceled), http.StatusRequestTimeout},
-		{simplerr.New("something").Code(simplerr.CodeConstraintViolated), http.StatusInternalServerError},
-		{fmt.Errorf("wrapped: %w", simplerr.New("something").Code(simplerr.CodeUnauthenticated)), http.StatusUnauthorized},
-		{fmt.Errorf("opaque: %s", simplerr.New("something").Code(simplerr.CodeUnauthenticated)), http.StatusInternalServerError},
-		{simplerr.Wrap(simplerr.New("something").Code(simplerr.CodePermissionDenied)), http.StatusForbidden},
-		{nil, 200}, // default code for httptest.ResponseRecorder is 200
+		{fmt.Errorf("something"), http.StatusInternalServerError, false},
+		{simplerr.New("something").Code(simplerr.CodeUnknown), http.StatusInternalServerError, false},
+		{simplerr.New("something").Code(simplerr.CodePermissionDenied), http.StatusForbidden, true},
+		{simplerr.New("something").Code(simplerr.CodeCanceled), http.StatusRequestTimeout, true},
+		{simplerr.New("something").Code(simplerr.CodeConstraintViolated), http.StatusInternalServerError, false},
+		{fmt.Errorf("wrapped: %w", simplerr.New("something").Code(simplerr.CodeUnauthenticated)), http.StatusUnauthorized, true},
+		{fmt.Errorf("opaque: %s", simplerr.New("something").Code(simplerr.CodeUnauthenticated)), http.StatusInternalServerError, false},
+		{simplerr.Wrap(simplerr.New("something").Code(simplerr.CodePermissionDenied)), http.StatusForbidden, true},
+		{nil, 200, false}, // default code for httptest.ResponseRecorder is 200
 	}
 
 	// Alter the default mapping
 	m := DefaultMapping()
 	m[simplerr.CodeCanceled] = http.StatusRequestTimeout
 	SetMapping(m)
-	SetDefaultErrorCode(http.StatusInternalServerError)
+	SetDefaultErrorStatus(http.StatusInternalServerError)
 
 	for ii, tc := range testCases {
 		r := httptest.NewRecorder()
 		SetStatus(r, tc.err)
+
+		// Check that GetStatus returns a status when there is a mapping
+		gotStatus, mappingFound := GetStatus(tc.err)
+		require.Equal(t, tc.expectMappingFound, mappingFound, fmt.Sprintf("test case %d failed", ii))
+		if mappingFound {
+			require.Equal(t, tc.expected, gotStatus, fmt.Sprintf("test case %d failed", ii))
+		}
 
 		require.Equal(t, tc.expected, r.Code, fmt.Sprintf("test case %d failed", ii))
 	}
