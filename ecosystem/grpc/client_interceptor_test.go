760c2ca4b354d4cf4bb7836c3e80d84d5ff71229
Calvin Lobo <calvinlobo@gmail.com>
2025-06-04 12:34:50 -0400

    Removed Ping grpc server. Mock the invoker layer now. This reduces dependencies. (#17)
    Upgrade golangci-lint to v2
    Fixed lint issue to make it pass again
    
    Co-authored-by: Calvin Lobo <calvin.lobo@hootsuite.com>

--- ecosystem/grpc/client_interceptor_test.go
+++ ecosystem/grpc/client_interceptor_test.go
@@ -4,46 +4,20 @@ import (
 	"context"
 	"fmt"
 	"github.com/lobocv/simplerr"
-	"github.com/lobocv/simplerr/ecosystem/grpc/internal/ping"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
-	"net"
 	"testing"
 )
 
-type PingService struct {
-	err error
+var mockInvoker = func(grpcError error) grpc.UnaryInvoker {
+	return func(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+		return grpcError
+	}
 }
 
-func (s *PingService) Ping(_ context.Context, _ *ping.PingRequest) (*ping.PingResponse, error) {
-	// Your implementation of the Ping method goes here
-	fmt.Println("Received Ping request")
-	if s.err != nil {
-		return nil, s.err
-	}
-	return &ping.PingResponse{}, nil
-}
-
-func setupServerAndClient(port int) (*PingService, ping.PingServiceClient) {
-
-	server := grpc.NewServer()
-	service := &PingService{err: status.Error(codes.NotFound, "test error")}
-	ping.RegisterPingServiceServer(server, service)
-
-	// Create a listener on TCP port 50051
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		panic(fmt.Sprintf("Error creating listener: %v", err))
-	}
-
-	go func() {
-		if err = server.Serve(listener); err != nil {
-			panic(fmt.Sprintf("Error serving: %v", err))
-		}
-	}()
+func makeMockGrpcCall(returnedError error) func() error {
 
 	defaultInverseMapping := DefaultInverseMapping()
 	defaultInverseMapping[codes.DataLoss] = simplerr.CodeResourceExhausted
@@ -51,22 +25,14 @@ func setupServerAndClient(port int) (*PingService, ping.PingServiceClient) {
 
 	interceptor := ReturnSimpleErrors(nil)
 
-	conn, err := grpc.NewClient(fmt.Sprintf(":%d", port),
-		grpc.WithUnaryInterceptor(interceptor),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		panic(err)
+	return func() error {
+		return interceptor(context.Background(), "/ping.PingService/Ping", nil, nil, nil, mockInvoker(returnedError))
 	}
-	client := ping.NewPingServiceClient(conn)
-
-	return service, client
 }
 
 func TestClientInterceptor(t *testing.T) {
 
-	server, client := setupServerAndClient(50051)
-	_, err := client.Ping(context.Background(), &ping.PingRequest{})
+	err := makeMockGrpcCall(status.Error(codes.NotFound, "not found"))()
 
 	require.True(t, simplerr.HasErrorCode(err, simplerr.CodeNotFound), "simplerror code can be detected")
 	require.Equal(t, codes.NotFound, status.Code(err), "grpc code can be detected with grpc status package")
@@ -80,8 +46,7 @@ func TestClientInterceptor(t *testing.T) {
 	require.Equal(t, "/ping.PingService/Ping", method, "can get the grpc method which errored")
 
 	// Test the custom added mapping
-	server.err = status.Error(codes.DataLoss, "test error")
-	_, err = client.Ping(context.Background(), &ping.PingRequest{})
+	err = makeMockGrpcCall(status.Error(codes.DataLoss, "data loss"))()
 	require.True(t, simplerr.HasErrorCode(err, simplerr.CodeResourceExhausted), "simplerror code can be detected")
 
 }
@@ -90,17 +55,10 @@ func TestClientInterceptor(t *testing.T) {
 // Our interceptor should still be able to detect attributes on the error
 func TestClientInterceptorNotGPRCError(t *testing.T) {
 
-	server, client := setupServerAndClient(50052)
-	server.err = fmt.Errorf("not a grpc error")
-
-	_, err := client.Ping(context.Background(), &ping.PingRequest{})
+	err := makeMockGrpcCall(fmt.Errorf("some error"))()
 
 	require.True(t, simplerr.HasErrorCode(err, simplerr.CodeUnknown), "simplerror code can be detected")
 	require.Equal(t, codes.Unknown, status.Code(err), "grpc code can be detected with grpc status package")
-
-	st, ok := simplerr.GetAttribute(err, AttrGRPCStatus)
-	require.True(t, ok)
-	require.Equal(t, codes.Unknown, st.(*status.Status).Code(), "can get the grpc Status") // nolint: errcheck
 
 	method, ok := simplerr.GetAttribute(err, AttrGRPCMethod)
 	require.True(t, ok)
@@ -109,9 +67,6 @@ func TestClientInterceptorNotGPRCError(t *testing.T) {
 }
 
 func TestClientInterceptorNoError(t *testing.T) {
-	server, client := setupServerAndClient(50053)
-	server.err = nil
-
-	_, err := client.Ping(context.Background(), &ping.PingRequest{})
+	err := makeMockGrpcCall(nil)()
 	require.Nil(t, err)
 }
